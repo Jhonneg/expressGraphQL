fd66b810eeb370d0f3d1bad663637803e8a7fe58
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-08-26 01:09:52 -0300

    node express example initial code

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 import express from "express";
+import { buildSchema } from "graphql";
+import { createYoga } from "graphql-yoga";
+
+const schema = buildSchema(`
+  type Query {
+   description: String
+   price:Float
+}
+`);
+
 
 const app = express();
+
+app.use(
+  "/graphql",
+  createYoga({
+    schema: schema,
+  })
+);
 
 app.listen(3000, () => {
   console.log("Running GraphQL server...");
