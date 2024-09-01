0531c4d600057260fc9e620206167dea4ddf3c4b
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-09-01 09:35:22 -0300

    added gql resolvers and models

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-import express from "express";
-import { createYoga } from "graphql-yoga";
-import { makeExecutableSchema } from "@graphql-tools/schema";
+const express = require("express");
+const { createYoga } = require("graphql-yoga");
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 
-const schemaText = `
-  type Query {
-    products: [Product]
-    order: [Order]
-}
-`;
+const typesArray = loadFilesSync("**/*,", {
+  extensions: ["graphql"],
+});
+
+const resolversArray = loadFilesSync("**/*", {
+  extensions: ["resolvers.js"],
+});
 
 const schema = makeExecutableSchema({
-  typeDefs: [schemaText],
+  typeDefs: typesArray,
+  resolvers: resolversArray,
 });
 
 const app = express();
